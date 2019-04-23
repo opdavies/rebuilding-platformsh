c81c9cb4008129e8a0c021da7180fed02ff319bc
Oliver Davies <oliver@oliverdavies.uk>
2019-04-24 00:42:05 +0100

    Add tailwindcss-interaction-variants plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+require('tailwindcss-interaction-variants')
+
+const defaultConfig = require('tailwindcss/defaultConfig')
+
 module.exports = {
   theme: {
     extend: {
@@ -6,5 +10,11 @@ module.exports = {
         inherit: 'inherit'
       }
     }
-  }
+  },
+  variants: {
+    backgroundColor: [...defaultConfig.variants.backgroundColor, 'hocus', 'group-hocus']
+  },
+  plugins: [
+    require('tailwindcss-interaction-variants')(),
+  ]
 }
